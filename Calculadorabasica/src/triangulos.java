3ffb48e4ff57fac9bd5f56dcc5915c3137193f2f
tucuch <TAURO95_oscar@hotmail.com>
2016-07-06 13:57:00 -0500

    trabajo de segunda oportunidad
    en es una calculadora  basica que realice para poder aprobar mi segunda oportunidad de la primera unidad

---
+++ Calculadorabasica/src/triangulos.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author oscar
+ */
+public class triangulos {
+//Creamos el método Main que es por donde inicia y finaliza un programa java.
+    
+}
